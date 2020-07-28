5171a245ed1492ba183209c94b0f924ef5169e3b
Katheryn Osmond <katheryn@katheryns-mbp.myfiosgateway.com>
2020-07-28 17:10:07 -0400

    Styling adjustments

--- src/pages/EmployeeForm/BioForm/BioForm.tsx
+++ src/pages/EmployeeForm/BioForm/BioForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Grid } from "@material-ui/core";
+import { Box, Card, Grid } from "@material-ui/core";
 import CheckboxField from "../Components/CheckboxField";
 import InputField from "../Components/InputField";
 import RadioButtonField from "../Components/RadioButtonField";
@@ -24,60 +24,62 @@ export const BioForm: React.FC<{
   const withFormGroup = addFormGroup(formGroup);
 
   return (
-    <Card>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={5}>
-          <InputField
-            name={withFormGroup(firstName.name)}
-            label={firstName.label}
-            fullWidth
-          />
+    <Box p={3}>
+      <Card>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={5}>
+            <InputField
+              name={withFormGroup(firstName.name)}
+              label={firstName.label}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <InputField
+              name={withFormGroup(middleInitial.name)}
+              label={middleInitial.label}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={5}>
+            <InputField
+              name={withFormGroup(lastName.name)}
+              label={lastName.label}
+              fullWidth
+            />
+          </Grid>
+          {/* Will be updated with correct field type */}
+          <Grid item xs={12} sm={6}>
+            <InputField
+              name={withFormGroup(birthDate.name)}
+              label={birthDate.label}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <RadioButtonField
+              name={withFormGroup(gender.name)}
+              data={GENDER}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <SelectField
+              name={withFormGroup(ethnicity.name)}
+              label={ethnicity.label}
+              data={ETHNICITY}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <CheckboxField
+              label={isCitizen.label}
+              name={withFormGroup(isCitizen.name)}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={2}>
-          <InputField
-            name={withFormGroup(middleInitial.name)}
-            label={middleInitial.label}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={5}>
-          <InputField
-            name={withFormGroup(lastName.name)}
-            label={lastName.label}
-            fullWidth
-          />
-        </Grid>
-        {/* Will be updated with correct field type */}
-        <Grid item xs={12} sm={6}>
-          <InputField
-            name={withFormGroup(birthDate.name)}
-            label={birthDate.label}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <RadioButtonField
-            name={withFormGroup(gender.name)}
-            data={GENDER}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <SelectField
-            name={withFormGroup(ethnicity.name)}
-            label={ethnicity.label}
-            data={ETHNICITY}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <CheckboxField
-            label={isCitizen.label}
-            name={withFormGroup(isCitizen.name)}
-          />
-        </Grid>
-      </Grid>
-    </Card>
+      </Card>
+    </Box>
   );
 };
 
