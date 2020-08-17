0ede89f9def63750ac7c50950939629a1ef3a758
Andrew <aeason96@vt.edu>
2020-08-17 16:51:33 -0400

    Add skill to employee

--- src/services/Employee/EmployeeService.ts
+++ src/services/Employee/EmployeeService.ts
@@ -1,5 +1,6 @@
 import { IEmployee } from "../../models/Employee.interface";
 import { IBaseCrudService, BaseCrudService } from "../abstract/BaseCrudService";
+import { IEmployeeSkill } from "../../models/Skill.interface";
 
 export interface IEmployeeService extends IBaseCrudService<IEmployee> {
   getByEmail(email: string): Promise<IEmployee>;
@@ -12,5 +13,15 @@ export default class EmployeeService extends BaseCrudService<IEmployee>
   async getByEmail(email: string) {
     const res = await this.get();
     return res.data.filter((item) => item.contact.email === email)[0];
+  }
+
+  async addSkill(employee: IEmployee, skill: IEmployeeSkill) {
+    const updatedEmployee: IEmployee = {
+      bio: employee.bio,
+      contact: employee.contact,
+      id: employee.id,
+      skills: [...employee.skills, skill],
+    };
+    return this.update(updatedEmployee).then((res) => res.data);
   }
 }
