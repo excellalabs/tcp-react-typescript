a1ee8f2835dcdd8f9d567db25fcabca04e159f9e
brendoncaulkins <brendon.caulkins@excella.com>
2020-08-17 17:35:36 -0400

    Add Baseline Category Table without API Integrations

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,17 +1,52 @@
+import React, { useState } from "react";
+
 import CategoryForm from "./CategoryForm/CategoryForm";
 import CategoryTable from "./CategoryTable/CategoryTable";
 import { ICategory } from "../../models/Skill.interface";
-import React from "react";
+import categories from "../../__mocks__/data/category";
 
 const Categories: React.FC<{}> = () => {
+  // MOCK DATA, SHOULD COME FROM API
+  const skilledCategories = categories.map((c) => ({
+    ...c,
+    skillCount: Math.round(Math.random() * 10),
+  }));
+
+  const [categoryToEdit, setCategoryToEdit] = useState<ICategory | undefined>(
+    {} as ICategory
+  );
+
   const handleSubmit = (category: ICategory) => {
-    console.log("got this from CategoryForm: ", category);
+    if (category.id) {
+      // REPLACE WITH API CALL
+      console.log("Updating Category: ", category);
+    } else {
+      // REPLACE WITH API CALL
+      console.log("Adding Category: ", category);
+    }
   };
+
+  function handleEditCategory(id: number) {
+    // REPLACE WITH API CALL TO GET SKILL
+    setCategoryToEdit(categories.find((c) => c.id === id));
+  }
+
+  function handleDeleteCategory(id: number) {
+    // REPLACE WITH API CALL
+    console.log("deleteing: ", id);
+  }
 
   return (
     <div>
-      <CategoryForm submitCategory={handleSubmit}></CategoryForm>
-      <CategoryTable></CategoryTable>
+      <CategoryForm
+        categoryToEdit={categoryToEdit}
+        submitCategory={handleSubmit}
+      ></CategoryForm>
+      <CategoryTable
+        categories={skilledCategories}
+        editCategory={handleEditCategory}
+        deleteCategory={handleDeleteCategory}
+      ></CategoryTable>
     </div>
   );
 };
--- src/pages/Categories/CategoryTable/CategoryTable.tsx
+++ src/pages/Categories/CategoryTable/CategoryTable.tsx
@@ -1,7 +1,74 @@
+import { DataColumn, DataTable } from "../../../components/DataTable/DataTable";
+
+import { Button } from "@material-ui/core";
+import { ICategory } from "../../../models/Skill.interface";
 import React from "react";
 
-const CategoryTable: React.FC<{}> = () => {
-  return <div>Category Table here.</div>;
+export interface ICategoryWithSkillCount extends ICategory {
+  skillCount: number;
+}
+
+export type CategoryTableProps = {
+  categories: ICategoryWithSkillCount[];
+  editCategory: (id: number) => void;
+  deleteCategory: (id: number) => void;
+};
+
+const CategoryTable: React.FC<CategoryTableProps> = ({
+  categories,
+  editCategory,
+  deleteCategory,
+}) => {
+  const columns: DataColumn<ICategoryWithSkillCount>[] = [
+    {
+      propertyName: "id",
+      headerLabel: "",
+      isNumeric: false,
+      renderer: (data: ICategoryWithSkillCount) => (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => editCategory(data.id)}
+        >
+          EDIT
+        </Button>
+      ),
+    },
+    {
+      // Don't Touch
+      propertyName: "name",
+      headerLabel: "Category Name",
+      isNumeric: false,
+      renderer: (data: ICategoryWithSkillCount) => data.name,
+    },
+    {
+      propertyName: "skillCount",
+      headerLabel: "# of Skills",
+      isNumeric: true,
+      // TOOD: FIX THIS WITH REAL SKILL COUNT
+      renderer: (data: ICategoryWithSkillCount) => data.skillCount,
+    },
+    {
+      propertyName: "id",
+      headerLabel: "",
+      isNumeric: false,
+      renderer: (data: ICategoryWithSkillCount) => (
+        <Button color="secondary" onClick={() => deleteCategory(data.id)}>
+          DELETE
+        </Button>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <DataTable<ICategoryWithSkillCount>
+        columns={columns}
+        rows={categories}
+        initialSortProperty="name"
+      />
+    </>
+  );
 };
 
 export default CategoryTable;
