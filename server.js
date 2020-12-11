657c1e8beb15c646df91e2d6273a731ebf413da0
Alex Martin <alexander.martin@excella.com>
2020-12-11 12:19:16 -0500

    Fix express routing

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.use(express.static('build'))
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
