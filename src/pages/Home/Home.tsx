beac92ed491faaebc826b7e6b6c764b62c18e139
Rob Gonnella <robert.gonnella@excella.com>
2020-12-21 08:36:29 -0500

    EVC 3: Add beginnings of va header
    Right now this just includes the log in a full page width flex box

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import * as styles from './home.module.scss';
 
-const HomePage: React.FC<{}> = () => {
-  return <div>Home Page</div>;
+const logo = "https://prod-va-gov-assets.s3-us-gov-west-1.amazonaws.com/img/header-logo.png";
+
+const HomePage: React.FunctionComponent<{}> = () => {
+  return (
+    <header className={styles.header}>
+        <Link to="/home">
+          <img src={logo} alt="Go to VA.gov" />
+        </Link>
+    </header>
+  );
 };
+HomePage.displayName = 'HomePage';
 
 export default HomePage;
