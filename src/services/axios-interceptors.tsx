8db7c3f4f997cbabc02614eb3cbe1538fdcf6f4c
Alex Hoffman <alexhoffman617@gmail.com>
2021-01-03 19:00:59 -0500

    update interceptor token and remove employee get from user login since they are no longer linked

--- src/services/axios-interceptors.tsx
+++ src/services/axios-interceptors.tsx
@@ -10,7 +10,7 @@ const oauthInterceptorInfo: InterceptorInfo = {
             return new Promise((resolve, reject) => {
             const res = {
                 data: {
-                    access_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX25hbWUiOiJ1c2VyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sImV4cCI6MTYwOTM3MDI1OCwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImp0aSI6IjlhOGIwZjVhLTNjYTQtNGEzNC05MjFkLWYxYTM3M2EzM2U1MiIsImVtYWlsIjoiam9obkB3aW5jaGVzdGVyLmNvbSIsImNsaWVudF9pZCI6ImFwcCIsImlhdCI6MTYwOTM2NjY1OH0.hJgYWyGIsLwCC9sh_SSu-GKFp8vGtZ4MhC-95nzo48w"
+                    access_token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX25hbWUiOiJ1c2VyIiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sImV4cCI6MTYwOTcyMTg1NSwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImp0aSI6IjlhOGIwZjVhLTNjYTQtNGEzNC05MjFkLWYxYTM3M2EzM2U1MiIsImVtYWlsIjoiam9obkB3aW5jaGVzdGVyLmNvbSIsImNsaWVudF9pZCI6ImFwcCIsImlhdCI6MTYwOTM2NjY1OH0.Bc1xq2fW8aH80OJxdpYGFEMRf-YQzK6RW692VHPzCBA"
                 },
                 status: 200,
                 statusText: "OK",
