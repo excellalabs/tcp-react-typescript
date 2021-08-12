3d623d5a57ed5b7232b078370a6f3fccace67e7b
Devin Venetsanos <devinv@vt.edu>
2021-08-12 15:42:23 -0400

    Remove additional unneeded files

--- src/mocks/data/category.ts
+++
@@ -1,8 +0,0 @@
-import { ICategory } from '../../models/Skill.interface'
-
-export const agileSkillCategory: ICategory = { id: 1, name: 'Agile' }
-export const techSkillCategory: ICategory = { id: 2, name: 'Technology' }
-
-export const categories = [agileSkillCategory, techSkillCategory]
-
-export default categories
