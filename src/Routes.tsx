bfeb84b43f434a7a7e7877805df92c7ac315c5e8
Keith Center <keith@centerjustified.com>
2021-08-12 15:33:34 -0400

    Removing unused routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,10 @@
+import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
-import AdminRoute from './components/Route/AdminRoute/AdminRoute'
-import CategoriesPage from './pages/Categories/Categories'
-import EmployeeFormContainer from './pages/EmployeeForm/EmployeeFormContainer'
-import EmployeesPage from './pages/Employees/Employees'
+// import AdminRoute from './components/Route/AdminRoute/AdminRoute'
 import HomePage from './pages/Home/Home'
 import LoginComponent from './pages/Login/Login'
 import NotFound from './pages/NotFound/NotFound'
-import React from 'react'
-import SkillsPage from './pages/Skills/Skills'
 import UserRoute from './components/Route/UserRoute/UserRoute'
 import { useAuthState } from './context/AuthContext/AuthContext'
 import ForgeUsers from './components/Forge/ForgeUsers'
@@ -28,29 +24,6 @@ export const Routes = () => {
       <UserRoute exact path="/home" component={HomePage} />
       <Route exact path="/login" component={LoginComponent} />
       <Route exact path="/users" component={ForgeUsers} />
-      <AdminRoute
-        exact
-        path="/employee/add"
-        component={EmployeeFormContainer}
-      />
-      <UserRoute exact path="/employee/list" component={EmployeesPage} />
-      <UserRoute
-        exact
-        path="/employee/self"
-        render={({ match }) => <div>Manage Current User Skills</div>}
-      />
-      <AdminRoute
-        exact
-        path="/employee/:id"
-        component={EmployeeFormContainer}
-      />
-      <AdminRoute
-        exact
-        path="/employee/edit/:id"
-        component={EmployeeFormContainer}
-      />
-      <AdminRoute exact path="/admin/categories" component={CategoriesPage} />
-      <AdminRoute exact path="/admin/skills" component={SkillsPage} />
       <Route component={NotFound} />
     </Switch>
   )
